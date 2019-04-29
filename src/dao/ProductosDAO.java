b58d109017949de05f2b96cd26533fa805c6d53a
Antonio Rios <31370952+ariosi86@users.noreply.github.com>
2019-04-29 12:39:23 -0500

    Update ProductosDAO.java

--- src/dao/ProductosDAO.java
+++ src/dao/ProductosDAO.java
@@ -82,5 +82,21 @@ public Producto obtenerProductoPorId(Integer idProducto) {
 	
 }
 
+public List<Productos> obtenerProductoPorNombre(String nombre){
+	List<Productos> shop = new ArrayList<Productos>();
+	try {
+        Statement st = cn.createStatement();
+        ResultSet rs = st.executeQuery("select * from productos where nombre = '"+nombre+"' LIMIT 1");
+        while(rs.next()){
+            Productos p = new Productos(rs.getString(2),rs.getString(3),rs.getString(4),rs.getFloat(5));
+            System.out.println(p.getNombre()+":"+ p.getPrecio());
+            shop.add(p);
+        }
+    } catch (SQLException e) {
+        e.printStackTrace();
+    }
+	return shop;
+}
+
 
 }
