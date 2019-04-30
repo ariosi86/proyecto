da50560917d2680d91633585a5978ba95f80a9d6
Antonio Rios <31370952+ariosi86@users.noreply.github.com>
2019-04-29 20:05:53 -0500

    Update Carrito.java

--- src/modelo/Carrito.java
+++ src/modelo/Carrito.java
@@ -6,7 +6,7 @@ public class Carrito {
 	private String nombre;
 	private String descripcion;
 	private float precio;
-
+	private int cantidad;
 	
 	public int getIdProducto() {
 		return idProducto;
@@ -14,6 +14,14 @@ public class Carrito {
 	public void setIdProducto(int idProducto) {
 		this.idProducto = idProducto;
 	}
+	public int getCantidad() {
+		return cantidad;
+	}
+	public void setCantidad(int cantidad) {
+		this.cantidad = cantidad;
+	}
+	
+	
 	public String getNombre() {
 		return nombre;
 	}
@@ -32,14 +40,8 @@ public class Carrito {
 	public void setPrecio(float precio) {
 		this.precio = precio;
 	}
-	public void ins(Productos p) {
-		// TODO Auto-generated method stub
-		
-	}
-	public Object getIt() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+	
+	
 	
 	
 	
