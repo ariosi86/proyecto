57ffb14ee2b81cebc7371cd8ca1122256577d5d1
Antonio Rios <31370952+ariosi86@users.noreply.github.com>
2019-04-29 19:56:08 -0500

    Update CarritoServlet.java

--- src/controladores/CarritoServlet.java
+++ src/controladores/CarritoServlet.java
@@ -16,6 +16,7 @@ import javax.servlet.http.HttpServletResponse;
 import modelo.Carrito;
 import modelo.Productos;
 import bd.ConexionBD;
+import dao.*;
 import java.util.List;
 
 /**
@@ -26,9 +27,18 @@ public class CarritoServlet extends HttpServlet {
 private static final long serialVersionUID = 1L;
 
 protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String nombre = request.getParameter("producto");
+        String idProducto = request.getParameter("idProducto");
+	int cantidad = Integer.parseInt(request.getParameter("cantidad"));
+	int id=Integer.parseInt(idProducto);
 	ProductosDAO productosDAO= new ProductosDAO();
-	List<Productos> shop= productosDAO.obtenerProductoPorNombre(nombre);
+	Producto seleccionado= productosDAO.obtenerProductoPorId(id);
+	CarritoDao carritoDAO= new CarritoDao();
+	Usuario usuario=(Usuario) request.getSession().getAttribute("usuario");
+	
+	carritoDAO.agregarCarritoUsuario( usuario.getIdUsuario() ,seleccionado.getId(), cantidad,seleccionado.getPrecio()*cantidad);
+	
+	
+	
 	request.setAttribute("shop", shop);
 	//response.sendRedirect("inicio.jsp?addedto=success");
 	request.getRequestDispatcher("WEB-INF/jsp/inicio.jsp?addedto=success").forward(request, response);
